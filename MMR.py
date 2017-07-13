171cd2ec5fd56c231e279bc9c9ad163de9a12f40
ByteMonkey <byte0monkey@gmail.com>
2017-07-13 22:55:25 +1000

    Update calculator

--- MMR.py
+++ MMR.py
@@ -1,12 +1,26 @@
-opponent_rating = int(input("Opponent Rating: "))
-player_rating = int(input("Player Rating: "))
-score = int(input("Score: "))
-opponent_score = int(input("Opponent Score: "))
-expected_score = 1/(1+10**((opponent_rating-player_rating)/280))
-opponent_expected_score = 1/(1+10**((player_rating-opponent_rating)/280))
+def check(score):
+    if score > 2000:
+        return 2000
+    elif score < -15:
+        return -15
+    else:
+        return score
 
-output = player_rating + 32 * (score - expected_score)
-second_output = player_rating + 32 * (opponent_score - opponent_expected_score)
 
-print("Player new MMR:", output)
-print("Opponent new MMR:", second_output)
+def calculate():
+    player_rating = int(input("Player Rating: "))
+    opponent_rating = int(input("Opponent Rating: "))
+    score = int(input("Player Score: "))
+    opponent_score = int(input("Opponent Score: "))
+
+    expected_score = round(1 / (1 + 10 ** ((opponent_rating - player_rating) / 280)), 1)
+    opponent_expected_score = round(1 / (1 + 10 ** ((player_rating - opponent_rating) / 280)), 1)
+
+    output = player_rating + (32 * (score - expected_score))
+    second_output = opponent_rating + (32 * (opponent_score - opponent_expected_score))
+
+    print("\nPlayer new MMR:", check(output))
+    print("Opponent new MMR:", check(second_output))
+
+
+calculate()
